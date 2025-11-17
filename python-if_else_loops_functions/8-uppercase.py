33c2b2f6e3800378ae9275d7d89a1e6301a7942b
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-11-17 22:40:39 +0400

    task-9

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        print("{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c), end="")
+def uppercase(string):
+    for c in string:
+        print(
+            "{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c),
+            end=""
+        )
     print("")
 
