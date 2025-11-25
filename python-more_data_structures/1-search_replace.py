e5c3246a7698110e81f966d50ccf41697580f6b8
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-11-25 13:52:09 +0400

    task-2

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    new_list = []
+    for el in my_list:
+        if el == search:
+            new_list.append(replace)
+        else:
+            new_list.append(el)
+    return new_list
