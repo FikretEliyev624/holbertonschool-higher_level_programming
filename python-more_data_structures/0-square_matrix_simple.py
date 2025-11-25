a180887f4ace79893243de23959be055dc2124ea
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-11-25 13:04:46 +0400

    task-1

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix=[]
+    new_matrix = []
     for row in matrix:
-        new_row=[]
+        new_row = []
         for n in row:
             new_row.append(n**2)
         new_matrix.append(new_row)
