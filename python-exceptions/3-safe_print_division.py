af8cb83a6549ba27eb8bf99a6fe9edbda042785c
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-11-27 14:19:38 +0400

    task-4

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res=None
+    res = None
     try:
-        res=a/b
+        res = a / b
     except ZeroDivisionError:
-        res=None
+        res = None
     finally:
-         print("Inside result: {}".format(res))
+        print("Inside result: {}".format(res))
     return res
