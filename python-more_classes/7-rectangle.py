3525ce176b0ffe689e831e6e5d4b9e5d13f42552
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-01 16:15:36 +0400

    task---8

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -66,6 +66,6 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
-        """Print a message when an instance is deleted"""
+        """Print a message when an instance is deleted!"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
