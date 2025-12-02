6ae03ae82f06744cdb7b69f559b6afefdbfae1b8
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-02 21:19:45 +0400

    task-5

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Inherits_from
+"""
 def inherits_from(obj, a_class):
     """Return True if obj inherits from a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
