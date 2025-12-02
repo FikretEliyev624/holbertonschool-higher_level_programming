65b2fbd8163a5abaff035fc87a694b6deb1489a8
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-02 16:45:02 +0400

    task1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 class Square:
     def __init__(self, size):
         self.__size = size
+=======
+"""
+0-square module
+
+Bu modul Square adlı bir sinfi təyin edir.
+Square sinfi private atribut __size ilə işləyir.
+"""
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        # Private attribute __size saxlayır
+        self.__size = size
+
+>>>>>>> 1ca4bce (task1)
