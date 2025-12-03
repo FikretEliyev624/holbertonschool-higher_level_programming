c95b8516a3450416adae6d99451d024e88f810ad
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-03 18:52:07 +0400

    task-7

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Docstring for 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Docstring for BaseGeometry
+    """
+
+    def area(self):
+        """
+        Docstring for area
+
+        :param self: Description
+        """
+        raise Exception("area() is not implemented")
