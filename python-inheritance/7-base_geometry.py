2c66b168c71886b5ae6bb03b1a705a2a2677e736
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-04 14:40:34 +0400

    task-12

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Module 7-base_geometry.py: BaseGeometry class."""
+"""Task 7"""
 
 
 class BaseGeometry:
-    """Base class for geometry objects."""
+    """Task 7"""
 
     def area(self):
-        """Public instance method that is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is an integer greater than 0."""
-        if not isinstance(value, int) or isinstance(value, bool):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
