673e8c9bb109f9e8e567db0825ac4c2b67ebba2d
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-07 14:42:03 +0400

    task-8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,28 +5,23 @@ Docstring for 7-add_item
 import sys
 from pathlib import Path
 
-"""
-Import custom functions to save and load JSON files
-"""
+
+# Import custom functions to save and load JSON files
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
-"""
-Check if the JSON file exists
-"""
+
+# Check if the JSON file exists
 if Path(filename).exists():
     items = load_from_json_file(filename)
 else:
     items = []
 
-"""
-Add all command-line arguments (excluding the script name) to the list
-"""
+# Add all command-line arguments (excluding the script name) to the list
 items.extend(sys.argv[1:])
 
-"""
-Save the updated list back to the JSON file
-"""
+
+# Save the updated list back to the JSON file
 save_to_json_file(items, filename)
