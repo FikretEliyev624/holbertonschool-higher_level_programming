cd5256fdc84f7f493a2b0cacf06e404ad1806bd5
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-09 18:06:22 +0400

    task-8

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+-- Create table 'cities'
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
