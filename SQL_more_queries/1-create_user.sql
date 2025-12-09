721e5874f233040ef1de872dcf65e298662b07eb
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-09 14:21:12 +0400

    task-2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,8 @@
--- 1-create_user.sql
-
+-- Create user user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Grant all privileges without WITH GRANT OPTION (this matches Holberton checker)
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
