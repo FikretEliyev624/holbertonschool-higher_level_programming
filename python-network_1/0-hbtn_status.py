27c52f5eeb1bfe1428906834d9bfcf2e61b7b09a
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-10 17:43:37 +0400

    task-1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ with urllib.request.urlopen(url) as response:
 print("Body response:")
 print("\t- type: {}".format(type(body)))
 print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode('utf-8')))
+print("\t- utf8 content: {}".format(body.decode("utf-8")))
