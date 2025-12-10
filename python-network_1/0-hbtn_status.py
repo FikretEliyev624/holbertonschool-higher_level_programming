10b1fa84ba13e88115665cc4431c3cefb7a0473b
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-10 17:38:27 +0400

    task-1

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Fetches https://intranet.hbtn.io/status using urllib"""
+
+from urllib import request
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    with request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
