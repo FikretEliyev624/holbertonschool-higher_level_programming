67633e7065f27313c1f4fe6da44bce5109b55bd1
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-12-12 15:59:11 +0400

    task-5

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,26 +1,26 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-
-# Route for the Home Page
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-
-# Route for the About Page
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
-
-# Route for the Contact Page
-@app.route("/contact")
+@app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template('contact.html')
 
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        data = json.load(file)
+    return render_template('items.html', items=data['items'])
 
-if __name__ == "__main__":
-    # Run the app on port 5000 with debugging enabled
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
